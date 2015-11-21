92fa960ee14fce20887bd53f25cb40a1ad4ca0c0
Daniel Tittle <daniel.tittle@gmail.com>
2015-11-21 09:57:41 -0800

    Added minimal canvas functionality

--- scratchpad.go
+++ scratchpad.go
@@ -5,7 +5,20 @@ import (
 )
 
 func main() {
-    doc := js.Global.Get("document")
-    //Clear the page
-    doc.Get("body").Set("innerHTML", "")
+	doc := js.Global.Get("document")
+	//Clear the page
+	doc.Get("body").Set("innerHTML", "")
+	doc.Set("title", "Canvas Tests")
+
+	canvas := doc.Call("createElement", "canvas")
+	canvas.Set("height", "500")
+	canvas.Set("width", "500")
+	canvas.Set("id", "myCanvas")
+	doc.Get("body").Call("appendChild", canvas)
+
+	ctx := doc.Call("getElementById", "myCanvas").Call("getContext", "2d")
+	ctx.Set("fillStyle", "red")
+
+	ctx.Call("fillRect", 0, 0, 50, 50)
+
 }
