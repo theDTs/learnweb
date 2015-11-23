5d23ec37d61eeee168a6f99ef1144077e2b964e5
Daniel Tittle <daniel.tittle@gmail.com>
2015-11-23 09:16:59 -0800

    Begun adding game of life functionality

--- scratchpad.go
+++ scratchpad.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gopherjs/gopherjs/js"
 )
 
+const cellSize = 10
+
 func main() {
 	doc := js.Global.Get("document")
 	//Clear the page
@@ -16,9 +18,41 @@ func main() {
 	canvas.Set("id", "myCanvas")
 	doc.Get("body").Call("appendChild", canvas)
 
-	ctx := doc.Call("getElementById", "myCanvas").Call("getContext", "2d")
+	ctx := canvas.Call("getContext", "2d")
 	ctx.Set("fillStyle", "red")
 
 	ctx.Call("fillRect", 0, 0, 50, 50)
 
 }
+
+type Game struct {
+	Board
+}
+
+type Board struct {
+	 state, prevState [][]Cell
+	 Rows, Cols uint
+}
+
+func New(rows, cols uint) Board {
+	
+	var b Board
+	b.Rows, b.Cols = rows, cols
+	
+	b.state = make([][]Cell, rows+2) //+2 adds a border 1 Cell wide at the edge of the board for the purposes of counting
+	b.prevState = make([][]Cell, rows+2) //We'll get the state of each generation by looking at the previous generation
+	
+	//Make a blank board
+	for y := 0; y < len(state); y++ {
+		state[y] = make([]Cell, cols+2)
+		prevState[y] = make([]Cell, cols+2)
+	}
+	
+	return b
+}
+
+type Cell struct {
+	Alive bool 
+	Player int
+}
+
